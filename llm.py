31e4c2a3ee710a523f54370577d0e55147ed056b
kennychen126 <114520330+kennychen126@users.noreply.github.com>
2023-11-08 22:40:11 -0500

    Update llm.py

--- llm.py
+++ llm.py
@@ -1,34 +1,30 @@
 import cv2
 import openai
-import torch
 from langchain.vectorstores import Chroma
 import os
 from flask import Flask, request, jsonify, render_template, Response, make_response
 from flask_cors import CORS
-from huggingface_hub import login
-from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.prompts import PromptTemplate
 from langchain.memory import ConversationBufferMemory
 from langchain.llms import OpenAI
 from langchain.chains import ConversationalRetrievalChain
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 # add token
-# openai.api_key = os.getenv("OPENAI_API_Key")
-openai.api_key = ""
-DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
-#login(token=os.getenv("Huggingface"))
-login(token="")
+openai.api_key = "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"
+
 # setting for the app
 app = Flask(__name__)
 CORS(app)
 # setting for model
-embeddings = HuggingFaceInstructEmbeddings(cache_folder="./embeddings",
-                                           model_name="sentence-transformers/all-MiniLM-L6-v2",
-                                           model_kwargs={"device": DEVICE})
+embeddings = OpenAIEmbeddings(openai_api_key="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")
 db3 = Chroma(persist_directory="./chroma_db", embedding_function=embeddings)
 
 # prompt
-#########
+########
 prompt_template = """You are a safety trainer aiming to improve behavior safety of construction workers. Do the followings:
                       1-Read the document carefully and accurately
                       2-Read personal information and daily tasks
@@ -36,69 +32,123 @@ prompt_template = """You are a safety trainer aiming to improve behavior safety 
 
 
 {context}
-
-Question: {question}
-Personal Information: {background}
+Personal Information: {project}
 Daily Tasks: {tasks}
+Question: {question}
 Answer:"""
+
+prompt_template2 ="""
+You are a safety trainer for workers. Base on the const_worker,project and task below. Design a multiple choice questions with 4 options, mostly focused on essential stuff workers need to remember when they are working. Please use less than 100 words to explain the correct choice.
+Context:{context}
+Project:{field2}
+task:{field3}
+Please create a different question than the previous one
+History:{chat_history}
+Please answer in the following format
+Question:
+A: 
+B: 
+C: 
+D: 
+Right answer:
+Reason:
+
+"""
 
 #######
 PROMPT = PromptTemplate(
-    template=prompt_template, input_variables=["context", "question", "background", "tasks"]
+    template=prompt_template, input_variables=["context", "question", "project", "tasks"]
 )
-memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True, output_key='answer')
+PROMPT2 = PromptTemplate(
+    template=prompt_template2, input_variables=["context", "field1","field2","field3"]
+)
+###
+memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True, input_key="question",output_key='answer')
+memory2 = ConversationBufferMemory(memory_key='chat_history', return_messages=True, input_key="field3",output_key='answer')
 qa = ConversationalRetrievalChain.from_llm(
     llm=OpenAI(model_name="gpt-3.5-turbo-instruct", temperature=0.1, openai_api_key=openai.api_key),
     memory=memory,
-    retriever=db3.as_retriever(search_kwargs={"k": 4}),
+    retriever=db3.as_retriever(search_kwargs={"k": 2}),
     combine_docs_chain_kwargs={'prompt': PROMPT}
 )
+qa2 = ConversationalRetrievalChain.from_llm(
+    llm=OpenAI(model_name="gpt-3.5-turbo-instruct", temperature=0.5, openai_api_key=openai.api_key),
+    memory = memory2,
+    retriever=db3.as_retriever(search_kwargs={"k": 2}),
+    combine_docs_chain_kwargs={'prompt': PROMPT2}
+)
 
+def get_chatgpt4(question, user_projects, user_tasks):
+    temp = qa({"question": question, "project": user_projects, "tasks": user_tasks})['answer']
+    return temp
 
-def get_chatgpt4(user_message, user_background, user_tasks):
-    return qa({"question": user_message, "background": user_background, "tasks": user_tasks})['answer']
+def create_question(user_worker,user_projects,user_tasks):
+    temp2 = qa2({"question":"","field1": user_worker,"field2":user_projects,"field3":user_tasks})['answer']
+    temp3 = temp2.splitlines()
+    return temp3
 
+@app.route('/')
+def hello():
+    return 'Hello World'
 
-def get_chatllama(user_message):
-    return qa({"question": user_message})['answer']
-
-
-@app.route("/image", methods=["POST"])
-def index():
-    user_message = request.json.get("message")
-    if user_message is None:
-        return jsonify({"error": "No message provided"}), 400
-    response = openai.Image.create(
-        prompt=user_message,
-        n=1,
-        size="256x256",
-    )
-    return jsonify({"message": response["data"][0]["url"]})
-
-
+@app.route("/quiz", methods=["POST"])
+def question_gpt():
+    user_worker = request.json.get("const_worker")
+    user_projects = request.json.get("projects")
+    user_tasks = request.json.get("tasks")
+    for i in range(10):
+        try:
+            response_message = create_question(user_worker,user_projects,user_tasks)
+            temp3 = []
+            for i in response_message:
+                if i != '':
+                    temp3.append(i)
+            print(temp3)
+            a0 = temp3[0].split(': ', 1 )[1]
+            a1 = temp3[1].split(': ', 1 )[1]
+            a2 = temp3[2].split(': ', 1 )[1]
+            a3 = temp3[3].split(': ', 1 )[1]
+            a4 = temp3[4].split(': ', 1 )[1]
+            a5 = temp3[5].split(': ', 1 )[1]
+            a6 = temp3[6].split(': ', 1 )[1]
+        except:
+            continue
+        break
+    
+    return jsonify({"question": a0,"A":a1,"B":a2,"C":a3,"D":a4,"Ans":a5,"res":a6})
+    
 @app.route("/chat_gpt4", methods=["POST"])
 def chat_gpt4():
-    user_message = request.json.get("message")
-    user_background = request.json.get("background")
-    user_tasks = request.json.get("daily_tasks")
-    if user_message is None:
-        return jsonify({"error": "No message provided"}), 400
+    question =  request.json.get("message")
+    user_worker = request.json.get("const_worker")
+    user_projects = request.json.get("projects")
+    user_tasks = request.json.get("tasks")
+    user_injury = request.json.get("injury")
+    user_start = request.json.get("start_t")
+    user_end = request.json.get("end_t")
 
-    response_message = get_chatgpt4(user_message, user_background, user_tasks)
-
+    response_message = get_chatgpt4(question, user_projects, user_tasks)
     return jsonify({"message": response_message})
 
+@app.route("/intro", methods=["POST"])
+def intro():
+    user_worker = request.json.get("const_worker")
+    user_projects = request.json.get("projects")
+    user_tasks = request.json.get("tasks")
+    user_injury = request.json.get("injury")
+    user_start = request.json.get("start_t")
+    user_end = request.json.get("end_t")
+    temp = ""
+    if "Yes" in user_worker:
+        if "Yes" in user_injury:
+            temp = "I'm a construction worker in "+user_projects+" projects. My upcoming task is "+user_tasks+". I have an injury experience in with "+user_injury+". I'm woking on this task "+user_start+" to "+user_end
+        else:
+            temp = "I'm a construction worker in "+user_projects+" projects. My upcoming task is "+user_tasks+". I'm woking on this task "+user_start+" to "+user_end
+        response2 = get_chatgpt4("Can you give me training based off of the info provided?", user_projects, user_tasks)
+        return jsonify({"message": temp + response2})
 
-@app.route("/chat_llame2", methods=["POST"])
-def chat_llame2():
-    user_message = request.json.get("message")
-    if user_message is None:
-        return jsonify({"error": "No message provided"}), 400
-
-    response_message = get_chatllama(user_message)
-
-    return jsonify({"message": response_message})
-
+    else:
+        return jsonify({"message": "I only assist construction workers, contact other specialists for geting safety advice in other industries"})
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5011)
+    app.run(host = '0.0.0.0', port=5011)
