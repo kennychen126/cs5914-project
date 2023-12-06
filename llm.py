d9d60c3ded0ecfa69357b05e1f47ddc4888a2c61
kennychen126 <114520330+kennychen126@users.noreply.github.com>
2023-12-06 16:07:57 -0500

    Update llm.py
    new version

--- llm.py
+++ llm.py
@@ -25,130 +25,237 @@ db3 = Chroma(persist_directory="./chroma_db", embedding_function=embeddings)
 
 # prompt
 ########
-prompt_template = """You are a safety trainer aiming to improve behavior safety of construction workers. Do the followings:
-                      1-Read the document carefully and accurately
-                      2-Read personal information and daily tasks
-                      3-Don't make general information. Match the personal information and daily tasks with safety regulations in the document and inform worker about safety protocols, numbers, and regulations.
+# PROMPT4 = PromptTemplate.from_template(
+# """
+# History:{chat_history}
+# You are a safety trainer. Create an open-ended dangerous scenario whick interacts with me based on the following steps: 
+# 1- My task is {input} and the related regulation to my task is mentioned in the following regulation section 
+# regulation: {context}
+# 2-Create a scenario following features: a) maximum 100 words and no questions b) different from the History c) in a way that create doubts to me that what I have to do 
+# 3-Begin your answer with ""Imagine as a construction worker, you are tasked to ..."
+
+# Return the answer as a JSON object:
+# {{
+#  "Scenario" : String
+# }}
+# """
+# )
 
 
-{context}
-Personal Information: {project}
-Daily Tasks: {tasks}
-Question: {question}
-Answer:"""
+PROMPT4 = PromptTemplate.from_template(
+"""
+You are a safety trainer. Follow these steps:
+1- If History is empty, create a scenario simulating a dangerous situation based on the following:
+   1-a- Your task is {input}, and the related regulation is mentioned in the following section:
+        Regulation: {context}
+   1-b- Keep it within 100 words and avoid questions.
+   1-c- Create ambiguity about what actions need to be taken.
+   1-d- Begin your answer with "Imagine as a construction worker, you are tasked to..."
 
-prompt_template2 ="""
-You are a safety trainer for workers. Base on the const_worker,project and task below. Design a multiple choice questions with 4 options, mostly focused on essential stuff workers need to remember when they are working. Please use less than 100 words to explain the correct choice.
-Context:{context}
-Project:{field2}
-task:{field3}
-Please create a different question than the previous one
-History:{chat_history}
-Please answer in the following format
-Question:
-A: 
-B: 
-C: 
-D: 
-Right answer:
-Reason:
+2- If History is not empty, continue the previous scenario based on the following:
+   2-a- The user's opinion regarding the first scenario is .
+   2-b- If the user's opinion is correct, introduce a different dangerous situation.
+   2-c- If the user's opinion is incorrect, consider the user's fault and the required regulations.
+   2-d- Begin your answer with "Let's continue the previous scenario..."
+
+Previously Scenario History:
+{history}
+
+Return the answer as a JSON object:
+{{
+  "Scenario": String
+}}
+"""
+)
+
+
+PROMPT5 = PromptTemplate.from_template(
+"""
+create three challenging options to assess my safety behavior in the following scenario: 
+scenario: {input}
+
+Return the answer as a JSON object:
+{{
+    "A": String
+    "B": String
+    "C": String
+    "Right_Answer": Uppercase Character
+}}
 
 """
-
-#######
-PROMPT = PromptTemplate(
-    template=prompt_template, input_variables=["context", "question", "project", "tasks"]
 )
-PROMPT2 = PromptTemplate(
-    template=prompt_template2, input_variables=["context", "field1","field2","field3"]
+PROMPT6 = PromptTemplate.from_template(
+"""
+Scenario:{scenario}
+OptionA:{optiona}
+OptionB:{optionb}
+OptionC:{optionc}
+My choice is {choice}
+Context: {context}
+Right Answer: {rightanswer}
+1-if My choice is correct only encourage me for another scenario
+1-if I'm wrong give me a feedback considering my choice with the aim of improving my behavior based on Right Answer
+2-Answer is 150 words
+3-Start with "Your choice was ..."
+
+Return the answer as a JSON object:
+{{
+    "Feedback": String
+}}
+"""
 )
-###
-memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True, input_key="question",output_key='answer')
-memory2 = ConversationBufferMemory(memory_key='chat_history', return_messages=True, input_key="field3",output_key='answer')
-qa = ConversationalRetrievalChain.from_llm(
-    llm=OpenAI(model_name="gpt-3.5-turbo-instruct", temperature=0.1, openai_api_key=openai.api_key),
-    memory=memory,
-    retriever=db3.as_retriever(search_kwargs={"k": 2}),
-    combine_docs_chain_kwargs={'prompt': PROMPT}
-)
-qa2 = ConversationalRetrievalChain.from_llm(
-    llm=OpenAI(model_name="gpt-3.5-turbo-instruct", temperature=0.5, openai_api_key=openai.api_key),
-    memory = memory2,
-    retriever=db3.as_retriever(search_kwargs={"k": 2}),
-    combine_docs_chain_kwargs={'prompt': PROMPT2}
+PROMPT8 = PromptTemplate.from_template(
+"""
+History:{chat_history}
+Scenario:{scenario}
+Feedback:{feedback}
+
+You can use imformation above to Answer Human Input
+Human Input{input}
+
+Return the answer as a JSON object:
+{{
+    "AI":String
+}}
+"""
 )
 
-def get_chatgpt4(question, user_projects, user_tasks):
-    temp = qa({"question": question, "project": user_projects, "tasks": user_tasks})['answer']
-    return temp
+memory = ConversationBufferWindowMemory(k=5,memory_key='chat_history',input_key='input')
+memory2 = ConversationBufferWindowMemory(k=5,memory_key='chat_history',input_key='input')
 
-def create_question(user_worker,user_projects,user_tasks):
-    temp2 = qa2({"question":"","field1": user_worker,"field2":user_projects,"field3":user_tasks})['answer']
-    temp3 = temp2.splitlines()
-    return temp3
+new1 = LLMChain(
+    #llm=OpenAI(model_name="gpt-3.5-turbo-instruct", temperature=0.5, openai_api_key=openai.api_key),
+    llm=ChatOpenAI(model_name='gpt-4', openai_api_key= openai.api_key, temperature = 0.5),
+    verbose=True,
+    prompt = PROMPT4
+    # memory=memory
+)
+new2 = LLMChain(
+    #llm=OpenAI(model_name="gpt-3.5-turbo-instruct", temperature=0.1, openai_api_key=openai.api_key),
+    llm=ChatOpenAI(model_name='gpt-4', openai_api_key= openai.api_key, temperature = 0.1),
+    verbose=True,
+    prompt = PROMPT5
+)
+new3 = LLMChain(
+    #llm=OpenAI(model_name="gpt-3.5-turbo-instruct", temperature=0.5, openai_api_key=openai.api_key),
+    llm=ChatOpenAI(model_name='gpt-4', openai_api_key= openai.api_key, temperature = 0.5),
+    verbose=True,
+    prompt = PROMPT6
+)
 
+new5 = LLMChain(
+    #llm=OpenAI(model_name="gpt-3.5-turbo-instruct", temperature=0.5, openai_api_key=openai.api_key),
+    llm=ChatOpenAI(model_name='gpt-4', openai_api_key= openai.api_key, temperature = 0.5),
+    verbose=True,
+    prompt = PROMPT8,
+    memory = memory2
+)
+###app part
 @app.route('/')
 def hello():
     return 'Hello World'
 
-@app.route("/quiz", methods=["POST"])
-def question_gpt():
-    user_worker = request.json.get("const_worker")
+retriever=db3.as_retriever(search_kwargs={"k": 2})
+@app.route("/scenario", methods=["POST"])
+def scenario():
     user_projects = request.json.get("projects")
-    user_tasks = request.json.get("tasks")
+    historys = request.json.get("historys")
+    history = '\n'.join([f'{index}: {item}' for index, item in enumerate(historys, start=1)])
+    retrieved_docs = retriever.invoke("requirements and protection equipments for " + user_projects)
     for i in range(10):
         try:
-            response_message = create_question(user_worker,user_projects,user_tasks)
-            temp3 = []
-            for i in response_message:
-                if i != '':
-                    temp3.append(i)
-            print(temp3)
-            a0 = temp3[0].split(': ', 1 )[1]
-            a1 = temp3[1].split(': ', 1 )[1]
-            a2 = temp3[2].split(': ', 1 )[1]
-            a3 = temp3[3].split(': ', 1 )[1]
-            a4 = temp3[4].split(': ', 1 )[1]
-            a5 = temp3[5].split(': ', 1 )[1]
-            a6 = temp3[6].split(': ', 1 )[1]
+            prompt1 = new1.predict(history = history,input=user_projects,context = retrieved_docs[0].page_content + "\n" + retrieved_docs[1].page_content)
+            loader = json.loads(prompt1)
+            result = jsonify({"scenario": loader["Scenario"]})
+            return result
         except:
             continue
         break
-    
-    return jsonify({"question": a0,"A":a1,"B":a2,"C":a3,"D":a4,"Ans":a5,"res":a6})
-    
-@app.route("/chat_gpt4", methods=["POST"])
-def chat_gpt4():
-    question =  request.json.get("message")
-    user_worker = request.json.get("const_worker")
+    return jsonify({"scenario": ""})
+
+@app.route("/options", methods=["POST"])
+def options():
+    prompt1 = request.json.get("prompt")
+    for i in range(10):
+        try:
+            response_message = new2.predict(input=prompt1)
+            loader = json.loads(response_message)
+            result = jsonify({"A": loader["A"],"B": loader["B"],"C": loader["C"],"Right_Answer": loader["Right_Answer"]})
+            return result
+        except:
+            continue
+        break
+    return jsonify({"A": "","B": "","C": "","Right_Answer": ""})
+
+@app.route("/combinetwo", methods=["POST"])
+def combinetwo():
     user_projects = request.json.get("projects")
-    user_tasks = request.json.get("tasks")
-    user_injury = request.json.get("injury")
-    user_start = request.json.get("start_t")
-    user_end = request.json.get("end_t")
+    historys = request.json.get("historys")
+    history = '\n'.join([f'{index}: {item}' for index, item in enumerate(historys, start=1)])
+    retrieved_docs = retriever.invoke("requirements and protection equipments for " +user_projects)
+    for i in range(10):
+        try:
+            prompt1 = new1.predict(history = history,input=user_projects,context = retrieved_docs[0].page_content + "\n" + retrieved_docs[1].page_content)
+            loader = json.loads(prompt1)
+            scenario = loader["Scenario"]
+        except:
+            continue
+        break
+    for i in range(10):
+        try:
+            response_message = new2.predict(input=scenario)
+            
+            loader2 = json.loads(response_message)
+            result2 = jsonify({"scenario": scenario,"A": loader2["A"],"B": loader2["B"],"C": loader2["C"],"Right_Answer": loader2["Right_Answer"]})
+            return result2
+        except:
+            continue
+        break
+    return jsonify({"scenario": "","A": "","B": "","C": "","Right_Answer": ""})
+    
 
-    response_message = get_chatgpt4(question, user_projects, user_tasks)
-    return jsonify({"message": response_message})
-
-@app.route("/intro", methods=["POST"])
-def intro():
-    user_worker = request.json.get("const_worker")
-    user_projects = request.json.get("projects")
-    user_tasks = request.json.get("tasks")
-    user_injury = request.json.get("injury")
-    user_start = request.json.get("start_t")
-    user_end = request.json.get("end_t")
-    temp = ""
-    if "Yes" in user_worker:
-        if "Yes" in user_injury:
-            temp = "I'm a construction worker in "+user_projects+" projects. My upcoming task is "+user_tasks+". I have an injury experience in with "+user_injury+". I'm woking on this task "+user_start+" to "+user_end
-        else:
-            temp = "I'm a construction worker in "+user_projects+" projects. My upcoming task is "+user_tasks+". I'm woking on this task "+user_start+" to "+user_end
-        response2 = get_chatgpt4("Can you give me training based off of the info provided?", user_projects, user_tasks)
-        return jsonify({"message": temp + response2})
-
+@app.route("/feedback", methods=["POST"])
+def feedback():
+    scenario = request.json.get("scenario")
+    optiona = request.json.get("optiona")
+    optionb = request.json.get("optionb")
+    optionc = request.json.get("optionc")
+    choice = request.json.get("choice")
+    right_answer= request.json.get("rightanswer")
+    if right_answer == "A": 
+        retrieved_docs = retriever.invoke(optiona)
+    elif right_answer == "B":
+        retrieved_docs = retriever.invoke(optionb)
     else:
-        return jsonify({"message": "I only assist construction workers, contact other specialists for geting safety advice in other industries"})
+        retrieved_docs = retriever.invoke(optionc)
+    
+    for i in range(10):
+        try:
+            response = new3.predict(rightanswer= right_answer, scenario = scenario,optiona=optiona,optionb = optionb,optionc = optionc,choice=choice,context = retrieved_docs[0].page_content + "\n" + retrieved_docs[1].page_content)
+            loader = json.loads(response)
+            result = jsonify({"Feedback": loader["Feedback"]})
+            return result
+        except:
+            continue
+        break
+    return jsonify({"Feedback": ""})
 
+@app.route("/chat", methods=["POST"])
+def chat():
+    scenario = request.json.get("scenario")
+    feedback = request.json.get("feedback")
+    input = request.json.get("input")
+    for i in range(10):
+        try:
+            response = new5.predict(scenario=scenario,feedback=feedback,input=input)
+            loader = json.loads(response)
+            result = jsonify({"AI": loader["AI"]})
+            return result
+        except:
+            continue
+        break
+    return jsonify({"AI": ""})
+    
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', port=5011)
+    
